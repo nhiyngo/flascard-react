0027ac5693fdfd849a90d81711f790a5ae97a0ae
nhi-ngo <nhingo.y@gmail.com>
2020-06-09 14:09:25 -0400

    add axios and fetch data

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,32 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import FlashcardList from './FlashcardList';
 import './app.css';
+import axios from 'axios';
 
 function App() {
   const [flashcards, setFlashcards] = useState(SAMPLE);
+
+  useEffect(() => {
+    getQuestions();
+  }, []);
+
+  const getQuestions = async () => {
+    const responses = await axios.get('https://opentdb.com/api.php?amount=10');
+
+    const questions = responses.data.results.map((questionItem, index) => {
+      const answer = questionItem.correct_answer;
+      const options = [...questionItem.incorrect_answers, answer];
+
+      return {
+        id: `${index}-${Date.now()}`,
+        question: questionItem.question,
+        answer,
+        options: options.sort(() => Math.random() - 0.5),
+      };
+    });
+
+    setFlashcards(questions);
+  };
 
   return (
     <FlashcardList flashcards={flashcards} />
