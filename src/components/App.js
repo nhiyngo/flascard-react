3dd4d9c42da8ae9db700195630d96a093a3e55a7
nhi-ngo <nhingo.y@gmail.com>
2020-06-09 16:09:12 -0400

    convert HTML encoded characters to normal string text

--- src/components/App.js
+++ src/components/App.js
@@ -1,25 +1,21 @@
 import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import FlashcardList from './FlashcardList';
 import './app.css';
-import axios from 'axios';
 
 function App() {
   const [flashcards, setFlashcards] = useState(SAMPLE);
-
-  useEffect(() => {
-    getQuestions();
-  }, []);
 
   const getQuestions = async () => {
     const responses = await axios.get('https://opentdb.com/api.php?amount=10');
 
     const questions = responses.data.results.map((questionItem, index) => {
       const answer = questionItem.correct_answer;
-      const options = [...questionItem.incorrect_answers, answer];
+      const options = [...questionItem.incorrect_answers, answer].map((a) => decodeString(a));
 
       return {
         id: `${index}-${Date.now()}`,
-        question: questionItem.question,
+        question: decodeString(questionItem.question),
         answer,
         options: options.sort(() => Math.random() - 0.5),
       };
@@ -27,6 +23,18 @@ function App() {
 
     setFlashcards(questions);
   };
+
+  useEffect(() => {
+    getQuestions();
+  }, []);
+
+  // to convert HTML encoded characters to normal string text
+  function decodeString(str) {
+    const textArea = document.createElement('textarea');
+    textArea.innerHTML = str;
+
+    return textArea.value;
+  }
 
   return (
     <FlashcardList flashcards={flashcards} />
